2eec381c62dda1556400196af8dbefa72438bb30
fananchong <fananchong@126.com>
2019-02-01 10:22:27 +0800

    修复"提示找不到 login_pb2 "

--- pyclient/protocol/cmd.py
+++ pyclient/protocol/cmd.py
@@ -1,6 +1,6 @@
 
 #! python3
 
-import login_pb2
+import protocol.login_pb2
 
-LOGIN = login_pb2._CMD_LOGIN_ENUM.values_by_name["LOGIN"].index
+LOGIN = protocol.login_pb2._CMD_LOGIN_ENUM.values_by_name["LOGIN"].index
