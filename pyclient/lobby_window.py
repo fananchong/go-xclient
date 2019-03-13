66d8862c24c16a19bb4064e2a45b0f3c680dda40
fananchong <fananchong@126.com>
2019-03-13 16:06:25 +0800

    聊天界面

--- pyclient/lobby_window.py
+++ pyclient/lobby_window.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import wx
+import protocol.lobby_pb2
+import log
+from server_type import *
+
+W = 600
+H = 400
 
 
 class LobbyWindow(wx.Frame):
@@ -15,21 +21,53 @@ class LobbyWindow(wx.Frame):
     def init(self):
         self.SetTitle(self.cfg["title"] + " - 大厅")
         self.Bind(wx.EVT_CLOSE, self.OnClose)
-        w = 1200
-        h = 800
-        self.SetMaxSize((w, h))
-        self.SetMinSize((w, h))
-        self.SetSize((w, h))
+        self.SetClientSize((W, H))
         self.init_window()
         self.Centre()
 
     def init_window(self):
-        wx.StaticText(self, -1, "账号：", pos=wx.Point(30, 15))
-        wx.StaticText(self, -1, self.cfg["login"]["account"],  pos=(70, 15))
-        wx.StaticText(self, -1, "角色ID：", pos=wx.Point(120, 15))
-        wx.StaticText(self, -1, str(self.user.current_role.id),  pos=(170, 15))
-        wx.StaticText(self, -1, "角色名：", pos=wx.Point(210, 15))
-        wx.StaticText(self, -1, self.user.current_role.name,  pos=(260, 15))
+        (w, h) = self.GetClientSize()
+
+        # 角色信息区
+        panelTopH = 50
+        panelTop = wx.Panel(self, -1, (1, 1), (w-2, panelTopH-2))
+        wx.StaticText(panelTop, -1, "账号：", (30, 15))
+        wx.StaticText(panelTop, -1, self.user.account,  (70, 15))
+        wx.StaticText(panelTop, -1, "角色ID：", (120, 15))
+        wx.StaticText(panelTop, -1, str(self.user.current_role.id), (170, 15))
+        wx.StaticText(panelTop, -1, "角色名：", (210, 15))
+        wx.StaticText(panelTop, -1, self.user.current_role.name, (260, 15))
+
+        # 聊天区
+        panelChatH = 180
+        panelChat = wx.Panel(
+            self, -1, (1, h - panelChatH+1), (w-2, panelChatH-2))
+        wx.StaticText(panelChat, -1, "聊天对象：", (30, 15))
+        self.chatTo = wx.TextCtrl(panelChat, -1, "", (100, 15))
+        wx.StaticText(panelChat, -1, "聊天内容：", (230, 15))
+        self.chatTxt = wx.TextCtrl(panelChat, -1, "", (300, 15), (w - 440, 25))
+        binChat = wx.Button(panelChat, -1, "发送", (w - 120, 15))
+        self.Bind(wx.EVT_BUTTON, self.OnChat, binChat)
+        self.chatHistory = wx.TextCtrl(
+            panelChat, -1, "", (30, 50), (w - 62, panelChatH - 60))
+        self.chatHistory.Enabled = False
+
+        # 功能区
+        panelFunction = wx.Panel(
+            self, -1, (1, panelTopH), (w-2, h - panelTopH - panelChatH))
+        binMatch = wx.Button(panelFunction, -1, "开始匹配",
+                             (0, 0), panelFunction.GetSize())
+        self.Bind(wx.EVT_BUTTON, self.OnMatch, binMatch)
+
+    def OnChat(self, evt):
+        log.info("开始发送聊天协议。 account: {0}".format(self.user.account))
+        msg = protocol.lobby_pb2.MSG_LOBBY_CHAT()
+        msg.To = self.chatTo.GetValue()
+        msg.Txt = self.chatTxt.GetValue()
+        self.user.clients[ServerType.Lobby].send(protocol.cmd.CHAT, msg)
+
+    def OnMatch(self, evt):
+        pass
 
     def OnClose(self, evt):
         self.user.close()
--- pyclient/protocol/cmd.py
+++ pyclient/protocol/cmd.py
@@ -19,3 +19,5 @@ CREATE_ROLE = LOBBY_MSGCMDOFFSET + \
     protocol.lobby_pb2._CMD_LOBBY_ENUM.values_by_name["CREATE_ROLE"].number
 ENTER_GAME = LOBBY_MSGCMDOFFSET + \
     protocol.lobby_pb2._CMD_LOBBY_ENUM.values_by_name["ENTER_GAME"].number
+CHAT = LOBBY_MSGCMDOFFSET + \
+    protocol.lobby_pb2._CMD_LOBBY_ENUM.values_by_name["CHAT"].number
